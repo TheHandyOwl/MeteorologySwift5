025cc51daaae74062e4af6d62b46ec32bc673e32
The Handy Owl <thehandyowl.io@gmail.com>
2020-05-03 23:54:47 +0200

    CLLocationManagerDelegate: GPS delegate

--- MeteorologySwift5/ViewController.swift
+++ MeteorologySwift5/ViewController.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
+import CoreLocation
+
 class ViewController: UIViewController {
+    
+    let locationManager = CLLocationManager()
 
     @IBOutlet weak var textCity: UITextField!
     @IBOutlet weak var labelCity: UILabel!
@@ -35,3 +39,46 @@ class ViewController: UIViewController {
     
 }
 
+extension ViewController : CLLocationManagerDelegate {
+
+    func getLocation() {
+            
+        locationManager.delegate = self
+        
+        // Permissions
+        if CLLocationManager.authorizationStatus() == .notDetermined {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
+        // Update frequency
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        // Location accuracy
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest // The best power, the most resources
+        //locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers // Low consumption, less resources
+        // Starting
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManagerDidPauseLocationUpdates(_ manager: CLLocationManager) {
+        
+        let locValue : CLLocationCoordinate2D = manager.location!.coordinate
+        
+        let latitude : Double = locValue.latitude
+        let longitude : Double = locValue.longitude
+
+        print("Latitude: \(latitude) and longitude: \(longitude)")
+        
+    }
+    
+    func locationManagerDidResumeLocationUpdates(_ manager: CLLocationManager) {
+        
+        let alertView = UIAlertController(title: "Error", message: "GPS does not work properly", preferredStyle: .alert)
+        
+        let button = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        
+        alertView.addAction(button)
+        dismiss(animated: true, completion: nil)
+
+    }
+    
+}
